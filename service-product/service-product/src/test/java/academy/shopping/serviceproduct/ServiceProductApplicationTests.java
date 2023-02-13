71212b149a3d1d9fc364a436c8e91885e1971b30
JulianDGP <juliandgp@hotmail.com>
2023-02-12 22:15:16 -0500

    serviceproduct

---
+++ service-product/service-product/src/test/java/academy/shopping/serviceproduct/ServiceProductApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.shopping.serviceproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
